7184e45ee3f47a59e88e916578b2f281ca89ffcb
liutao <zimult@163.com>
2018-05-03 21:23:38 +0800

    文章同步

--- python/qicycling/api.py
+++ python/qicycling/api.py
@@ -6,16 +6,19 @@
 from flask import Flask
 import config
 import db
-
+from flask import request
+import sync
 
 shorty_api = Flask(__name__)
 
 db_wp = db.DB(config.host, config.user, config.password, config.database_wp)
 db_app = db.DB(config.host, config.user, config.password, config.database_app)
 
-@shorty_api.route('/get_cartoon_material', methods=['GET', 'POST'])
-def get_cartoon_material():
-    return "haah"
+@shorty_api.route('/sync_article', methods=['GET', 'POST'])
+def sync_article():
+    article_id = int(request.values.get('article_id'))
+
+    return str(article_id)
 
 if __name__ == '__main__':
     shorty_api.run(host='0.0.0.0', port=8410 )
