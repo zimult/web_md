e79011282c09ed2b0d5b6523259f4d3c8ebbe1e0
liutao <zimult@163.com>
2018-05-12 17:54:44 +0800

    增加页面功能

---
+++ html/qicycling/js/qy/schedule_list.js
@@ -0,0 +1,56 @@
+var tasks = new Vue({
+    el: '#task_page',
+    data: {
+        match_list: [],
+    },
+    methods: {
+        
+    }
+});
+
+$(document).ready(function(){
+    //setDates()
+    $("#id-btn-list").click(function(e){
+    	//console.log('sss')
+        listMatch()
+        e.stopPropagation()
+        return
+    })
+    
+})
+
+// $("#id_input_st").change(function(){
+//     $("#id_input_st").attr("value",$(this).val()); //赋值
+// });
+function listMatch() {
+    st = $('#id_input_st').val()
+    et = $('#id_input_et').val()
+    if (st == '' || et == '') {
+       alert("请完成输入数据");
+       return;
+    }
+    list_match(st,et)
+};
+
+function list_match(st,et) {
+	waitingDialog.show("查询中...");
+	_list_match(st, et, function(params, data, error){
+        waitingDialog.hide()
+        if (error) {	
+            alert(error)
+        } else {
+            //alert("同步成功")
+        }
+    })
+}
+
+function _list_match(st, et, callback)	{
+    var params = {
+        "st": st,
+        "et": et
+    };
+    //var url = DOMAIN+"/wx/get_keyword_tj";
+    var url = "http://47.97.124.47:8410"+"/list_match";
+    //提交
+    _submit(url, "POST", params, callback);
+}
