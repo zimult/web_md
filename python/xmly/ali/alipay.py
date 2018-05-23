bbf70d91bf0b12604fb6347d242559bb29f7ad20
liutao <zimult@163.com>
2018-05-23 22:24:30 +0800

    请求验签函数

--- python/xmly/ali/alipay.py
+++ python/xmly/ali/alipay.py
@@ -3,15 +3,15 @@
 __author__ = 'zimult'
 
 from datetime import datetime
-# from Crypto.PublicKey import RSA
-# from Crypto.Signature import PKCS1_v1_5
-#from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 from base64 import b64encode, b64decode
 from urllib import quote_plus
 from urlparse import urlparse, parse_qs
 from urllib import urlopen
-#from base64 import decodebytes, encodebytes
-import rsa
+import base64
+#import rsa
 import json
 
 
@@ -29,12 +29,14 @@ class AliPay(object):
         self.return_url = return_url
 
         with open(self.app_private_key_path) as fp:
-            self.app_private_key = rsa.PrivateKey.load_pkcs1(fp.read())
+            #self.app_private_key = rsa.PrivateKey.load_pkcs1(fp.read())
+            self.app_private_key = RSA.importKey(fp.read())
 
         self.alipay_public_key_path = alipay_public_key_path
         with open(self.alipay_public_key_path) as fp:
-            print fp.read()
-            self.alipay_public_key = rsa.PublicKey.load_pkcs1(fp.read())
+            #print fp.read()
+            #self.alipay_public_key = rsa.PublicKey.load_pkcs1(fp.read())
+            self.alipay_public_key = RSA.importKey(fp.read())
 
         # pem_prefix = '-----BEGIN RSA PUBLIC KEY-----\r\n'
         # pem_suffix = '\r\n-----END RSA PUBLIC KEY-----'
@@ -87,10 +89,14 @@ class AliPay(object):
         unsigned_items = self.ordered_data(data)
         unsigned_string = "&".join("{0}=\"{1}\"".format(k, v) for k, v in unsigned_items)
         sign = self.sign(unsigned_string.encode("utf-8"))
-        print unsigned_items,unsigned_string,sign
+        print sign
+        #print unsigned_items,unsigned_string,sign
 
         ordered_items = self.ordered_data(data)
-        quoted_string = "&".join("{0}={1}".format(k, quote_plus(v)) for k, v in ordered_items)
+        print ordered_items
+        for k, v in ordered_items:
+            print k,v
+        quoted_string = "&".join("{0}=\"{1}\"".format(k, quote_plus(v)) for k, v in ordered_items)
         # 获得最终的订单信息字符串
         signed_string = quoted_string + "&sign=" + quote_plus(sign)
         return signed_string
@@ -110,8 +116,8 @@ class AliPay(object):
     def sign(self, unsigned_string):
         # 开始计算签名
         key = self.app_private_key
-        # signer = PKCS1_v1_5.new(key)
-        # signature = signer.sign(SHA256.new(unsigned_string))
+        signer = PKCS1_v1_5.new(key)
+        signature = signer.sign(SHA256.new(unsigned_string))
         # print unsigned_string
         #a = bytes(signature)
         #print type(signature)
@@ -120,8 +126,9 @@ class AliPay(object):
         # base64 编码，转换为unicode表示并移除回车
         #sign = encodebytes(signature).decode("utf8").replace("\n", "")
         #str2 =
-        print unsigned_string
-        r_sign = rsa.sign(unsigned_string, key, 'SHA-1')
+        r_sign = base64.b64encode(signature).decode("utf8").replace("\n", "")
+        # print unsigned_string
+        # r_sign = rsa.sign(unsigned_string, key, 'SHA-1')
         return r_sign
 
     def _verify(self, raw_content, signature):
@@ -155,8 +162,8 @@ if __name__ == "__main__":
     alipay = AliPay(
         appid="2018052160236073",
         app_notify_url="http://projectsedus.com/",
-        app_private_key_path="/Users/Admin/Downloads/应用私钥2048.txt",
-        alipay_public_key_path="/Users/Admin/Downloads/alipay_public_2048.txt",
+        app_private_key_path="./app_private_key_2048.txt",
+        alipay_public_key_path="./alipay_public_key_sha256.txt",
         # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
         debug=True,  # 默认False,
         return_url=""
@@ -164,13 +171,14 @@ if __name__ == "__main__":
 
     o = urlparse(return_url)
     query = parse_qs(o.query)
+    print query['sign'][0]
+    processed_query = {}
+    ali_sign = query.pop("sign")[0]
+    for key, value in query.items():
+        processed_query[key] = value[0]
+    alipay.sign_data(processed_query)
 
-    alipay.sign_data(query)
 
-    # processed_query = {}
-    # ali_sign = query.pop("sign")[0]
-    # for key, value in query.items():
-    #     processed_query[key] = value[0]
     #
     # print(alipay.verify(processed_query, ali_sign))
 
