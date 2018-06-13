140e8d99c47d66bca77ccd7d6a6fb8f54e411746
zimult <zimult@gmail.com>
2018-06-13 11:54:07 +0800

    debug

--- python/xmly/api.py
+++ python/xmly/api.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-#import logging
-#from logging.handlers import TimedRotatingFileHandler
+# import logging
+# from logging.handlers import TimedRotatingFileHandler
 
 from flask import Flask
 import config
-#import db
+# import db
 import requests
 import time
 from flask import request, redirect
@@ -27,23 +27,25 @@ shorty_api = Flask(__name__)
 appKey = 'f9aee5687ff7d8e66cdeff6a3c63c81a'
 appSecret = '4fd4c41d4fe59a183ffd106a61d895bc'
 
+
 def access_token2(code, device_id):
-    #url = "http://192.168.3.54:8206/oauth2-auth-app/v2/access_token"
+    # url = "http://192.168.3.54:8206/oauth2-auth-app/v2/access_token"
     url = "https://api.ximalaya.com/oauth2/v2/access_token"
 
     headers = {"Content-type": "application/x-www-form-urlencoded"}
-    body = {'grant_type':'authorization_code',
-            'redirect_uri':'https://app.yuan7dan.com/redirect_uri',
-            'client_secret':appSecret,
-            'client_id':appKey,
-            'device_id':device_id,
-            'code':code,
+    body = {'grant_type': 'authorization_code',
+            'redirect_uri': 'https://app.yuan7dan.com/redirect_uri',
+            'client_secret': appSecret,
+            'client_id': appKey,
+            'device_id': device_id,
+            'code': code,
             }
     print body
     test_data_urlencode = urllib.urlencode(body)
     request = urllib2.Request(url=url, headers=headers, data=test_data_urlencode)
     rt = urllib2.urlopen(request).read()
     print rt
+
 
 def access_token(code, device_id):
     url = "https://api.ximalaya.com/oauth2/v2/access_token"
@@ -59,10 +61,11 @@ def access_token(code, device_id):
     print r.text
     return r.text
 
+
 def refresh_token(device_id, refresh_token):
     url = "https://api.ximalaya.com/oauth2/refresh_token"
-    body = {'grant_type': 'refresh_token',
-            #'redirect_uri': 'http://api.test.ximalaya.com:8137/openapi-collector-app/get_access_token',
+    body = {'grant_type': 'authorization_code',
+            # 'redirect_uri': 'http://api.test.ximalaya.com:8137/openapi-collector-app/get_access_token',
             'redirect_uri': 'https://app.yuan7dan.com/redirect_uri',
             'client_secret': appSecret,
             'client_id': appKey,
@@ -73,27 +76,27 @@ def refresh_token(device_id, refresh_token):
     print r.text
     return r.text
 
+
 @shorty_api.route('/redirect_uri', methods=['GET', 'POST'])
 def redirect_uri():
     code = request.values.get('code')
     state = request.values.get('state')
     device_id = request.values.get('device_id')
-    #print id
+    # print id
 
     rt = access_token(code, device_id)
     rt_j = json.loads(rt)
     if rt_j.has_key('error_no'):
         return rt
 
-
-    # if rt_j.has_key('refresh_token'):
-    #     rf_token = rt_j['refresh_token']
-    #     print(rf_token)
-    #     rt2 = refresh_token(device_id, rf_token)
-    #     js = json.loads(rt2)
-    #     if js.has_key('error_no'):
-    #         print("refresh_token return [%s]"%rt2)
-    #         return rt2
+    if rt_j.has_key('refresh_token'):
+        rf_token = rt_j['refresh_token']
+        print(rf_token)
+        rt2 = refresh_token(device_id, rf_token)
+        js = json.loads(rt2)
+        if js.has_key('error_no'):
+            print("refresh_token return [%s]" % rt2)
+            return rt2
 
     print rt
     return rt
@@ -105,7 +108,7 @@ def get_trade_no():
 
 @shorty_api.route('/ali_order', methods=['GET', 'POST'])
 def ali_order():
-    #state = request.values.get('state')
+    # state = request.values.get('state')
     alipay = AliPay(
         appid="2018052160236073",
         app_notify_url="http://projectsedus.com/",
@@ -121,7 +124,7 @@ def ali_order():
     total_fee = request.values.get('total_fee')
     item_desc = request.values.get('item_desc')
     data = {}
-    data['partner']='2018052160236073'
+    data['partner'] = '2018052160236073'
     data['service'] = 'mobile.securitypay.pay'
     data['_input_charset'] = 'UTF-8'
     data['sign_type'] = 'RSA'
@@ -133,7 +136,6 @@ def ali_order():
     data['seller_id'] = '2088421398130544'
     data['total_fee'] = str(total_fee)
     data['body'] = item_desc
-
 
     print type(data)
 
@@ -152,12 +154,12 @@ def all_paid_albums():
     url = "https://api.ximalaya.com/open_pay/all_paid_albums"
     ts = int(round(time.time() * 1000))
     params = {
-        'page':1,
-        'count':20,
-        'app_key':'b617866c20482d133d5de66fceb37da3',
-        'client_os_type':4,
-        'nonce':'a3878a1a361229e0a34eb9931d229e69',
-        'timestamp':ts
+        'page': 1,
+        'count': 20,
+        'app_key': 'b617866c20482d133d5de66fceb37da3',
+        'client_os_type': 4,
+        'nonce': 'a3878a1a361229e0a34eb9931d229e69',
+        'timestamp': ts
     }
     sign = get_sign(params)
     params['sig'] = sign
@@ -168,18 +170,18 @@ def all_paid_albums():
     return res.text
 
 
-@shorty_api.route('/xmly', methods=['GET','POST'])
+@shorty_api.route('/xmly', methods=['GET', 'POST'])
 def xmly():
     dt = dict(request.args)
-    #print dt
+    # print dt
     params = {}
-    for k,v in dt.items():
+    for k, v in dt.items():
         params[k] = v[0]
-    #print params
+    # print params
     req_api = params['req_api']
     params.pop('req_api')
     params.pop('sig')
-    params['timestamp'] = int(round(time.time()*1000))
+    params['timestamp'] = int(round(time.time() * 1000))
 
     print params
     print request.method
@@ -197,4 +199,4 @@ def xmly():
 
 
 if __name__ == '__main__':
-    shorty_api.run(host='0.0.0.0', port=8000 )
+    shorty_api.run(host='0.0.0.0', port=8000)
