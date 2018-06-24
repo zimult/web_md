9467c0cb908142edcf1a0e6c15a30e68e6ac7349
liutao <zimult@163.com>
2018-06-24 14:28:57 +0800

    增加网站指定板块不同步的功能

--- python/qicycling/sync.py
+++ python/qicycling/sync.py
@@ -22,24 +22,28 @@ from urllib import unquote
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 def downloadPic(url):
     basename = os.path.basename(url)
-    #print basename
+    # print basename
     cmd = "curl " + url + " -o pic/" + basename
     os.system(cmd)
     return "pic/" + basename
+
 
 def getPicSize(file):
     img_ = Image.open(file)
     width, height = img_.size
     return width, height
 
+
 def getUrlPicSize(url):
     file = downloadPic(url)
     img_ = Image.open(file)
     width, height = img_.size
-    #os.remove(file)
+    # os.remove(file)
     return width, height
+
 
 def getBanner(pageUrl):
     try:
@@ -49,11 +53,11 @@ def getBanner(pageUrl):
         response = requests.get(pageUrl)
 
         html = response.text
-        #print html
+        # print html
 
         soup = BeautifulSoup(html, "html.parser")
         ca = soup.find('div', {'class': 'container-fluid pd0'})
-        #print ca
+        # print ca
 
         # banner = ca.select('[class="item"]')
         # for i in xrange(len(banner)):
@@ -77,16 +81,17 @@ def getBanner(pageUrl):
 
         banners = ca.findAll('a', {'class': 'permalink'})
         for banner in banners:
-            #print banner
+            # print banner
             href = banner.attrs['href']
             base = os.path.basename(href).split('.')[0]
-            #print base
+            # print base
             list.append(base)
 
         return list
     except Exception, e:
         print e
         print traceback.format_exc()
+
 
 def recordBanner_pic(db_app, banners):
     cursor = db_app.get_cursor()
@@ -95,13 +100,14 @@ def recordBanner_pic(db_app, banners):
             url = banner['banner']
             text = banner['text']
 
-            cursor.execute("SELECT id FROM banner where pic_url='%s' and title='%s'"%(url, text))
+            cursor.execute("SELECT id FROM banner where pic_url='%s' and title='%s'" % (url, text))
             result = cursor.fetchone()
             if result is None:
                 t = time.time()
                 ts = int(round(t * 1000))
-                sql = "INSERT INTO banner (pic_url, title, `timestamp`, `position`, `resource_type`) values ('%s', '%s', %d, 'index', 1)"%(url, text, ts)
-                #print sql
+                sql = "INSERT INTO banner (pic_url, title, `timestamp`, `position`, `resource_type`) values ('%s', '%s', %d, 'index', 1)" % (
+                    url, text, ts)
+                # print sql
                 cursor.execute(sql)
             else:
                 print "banner exists..."
@@ -111,13 +117,14 @@ def recordBanner_pic(db_app, banners):
         log.error(e.message)
         log.error(traceback.format_exc())
 
+
 def recordBanner(db_app, banners):
     cursor = db_app.get_cursor()
     try:
         modules = get_modules(db_app)
         str = ','.join(banners)
-        #print str
-        cursor.execute("DELETE FROM resource_banner where resource_id not in (%s)"%str)
+        # print str
+        cursor.execute("DELETE FROM resource_banner where resource_id not in (%s)" % str)
 
         for bn in banners:
             banner = int(bn)
@@ -126,12 +133,14 @@ def recordBanner(db_app, banners):
                 if module == 9:
                     status = 1
 
-                cursor.execute("SELECT id FROM resource_banner where `position`=%d and resource_id=%d"%(module, banner))
+                cursor.execute(
+                    "SELECT id FROM resource_banner where `position`=%d and resource_id=%d" % (module, banner))
                 result = cursor.fetchone()
                 if result is None:
                     t = time.time()
                     ts = int(round(t * 1000))
-                    sql = "INSERT INTO resource_banner (status, `timestamp`, `position`, resource_id) values (%d, %d,%d,%d)"%(status,ts,module,banner)
+                    sql = "INSERT INTO resource_banner (status, `timestamp`, `position`, resource_id) values (%d, %d,%d,%d)" % (
+                        status, ts, module, banner)
                     print sql
                     cursor.execute(sql)
                 else:
@@ -150,9 +159,10 @@ def get_modules(db_app):
     results = cursor.fetchall()
     for row in results:
         id = row[0]
-        #print id
+        # print id
         list.append(id)
     return list
+
 
 def getArticle(page):
     # curl -d 'paged=0&action=ajax_load_posts&append=home-list&page=home' http://www.qicycling.cn/wp-admin/admin-ajax.php
@@ -164,7 +174,7 @@ def getArticle(page):
     r = requests.post(url, data)
     html = r.text
     soup = BeautifulSoup(html, "html.parser")
-    #articles = soup.select('[class="image"]')
+    # articles = soup.select('[class="image"]')
     articles = soup.select('article')
     for article in articles:
         # print "*" * 12
@@ -175,7 +185,7 @@ def getArticle(page):
 
         images = article.select('[class="image"]')
         image = images[0]
-        #print image
+        # print image
         img = image.select('img')
 
         if len(img) <= 0:
@@ -189,7 +199,7 @@ def getArticle(page):
 
         contents = article.select('[class="content"]')
         chans = contents[0].select('a')
-	#print chans[0].text
+        # print chans[0].text
         a1['chan'] = ''
         if len(chans) == 2:
             print chans[1].text
@@ -202,13 +212,14 @@ def getArticle(page):
         article_id = os.path.basename(href).split('.')[0]
         a1['article_id'] = article_id
 
-        #print a1
+        print a1
         list.append(a1)
 
         ### debug break
-        #break
+        # break
 
     return list
+
 
 def get_href(url):
     # "Mozilla/5.0 (Linux; Android 7.1.1; OD103 Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.83 Mobile Safari/537.36"
@@ -220,9 +231,10 @@ def get_href(url):
     html = response.text
     return get_href_detail(html)
 
+
 def get_href_detail(html):
     soup = BeautifulSoup(html, "html.parser")
-    #print type(soup)
+    # print type(soup)
     soup.find('div', {'class': 'post-footer clearfix'}).extract()
 
     utime = soup.findAll('span', {'class': 'u-time'})
@@ -230,8 +242,8 @@ def get_href_detail(html):
     print len_utime
     for i in range(0, len_utime):
         soup.find('span', {'class': 'u-time'}).extract()
-    #print soup.prettify()
-    #print soup
+    # print soup.prettify()
+    # print soup
 
     html_head = soup.head.prettify()
     html_top = '' + html_head
@@ -243,7 +255,7 @@ def get_href_detail(html):
 
     title = ''
     t_ = soup.findAll('meta', {'property': 'og:title'})
-    if len(t_)>0:
+    if len(t_) > 0:
         title = t_[0].get('content')
 
     ######## no data
@@ -296,7 +308,7 @@ def get_href_detail(html):
         new_html += "<section class=\"nt-warp nt-warp-nospace\">\n" \
                     "<div class=\"container\">\n"
         cover = soup.findAll('div', {'class': 'suxing post-cover'})
-        print("cover len:%d"%len(cover))
+        print("cover len:%d" % len(cover))
         for i in xrange(len(cover)):
             html_article = cover[i].prettify()
             new_html += html_article
@@ -307,7 +319,7 @@ def get_href_detail(html):
         new_html += add_str
 
         article = soup.select('article')
-        #print("article len:%d" % len(article))
+        # print("article len:%d" % len(article))
         for i in xrange(len(article)):
             html_article = article[i].prettify()
             # print html_article
@@ -324,9 +336,9 @@ def get_href_detail(html):
 
     new_html += "</body>"
 
-    #footer = soup.find('div', {'class': 'post-footer clearfix'})
-    #footer_text = footer.prettify()
-    #print footer_text
+    # footer = soup.find('div', {'class': 'post-footer clearfix'})
+    # footer_text = footer.prettify()
+    # print footer_text
 
     author = soup.find('div', {'class': 'author-name'})
     author_name = ''
@@ -337,11 +349,11 @@ def get_href_detail(html):
     img_list = []
 
     # post-image
-    #srcsets = soup.findAll('div', {'class': 'post-image'})
+    # srcsets = soup.findAll('div', {'class': 'post-image'})
     srcsets = soup.select('article')
     for srcset in srcsets:
         imgs = srcset.findAll('img')
-        if len(imgs)<=0:
+        if len(imgs) <= 0:
             continue
         for img in imgs:
             img_url = img.get('src')
@@ -360,8 +372,8 @@ def get_href_detail(html):
                         log.error(traceback.format_exc())
                         continue
 
-            pinfo = {'url':img_url, 'width':int(width), 'height':int(height)}
-            if(int(width) > 100):
+            pinfo = {'url': img_url, 'width': int(width), 'height': int(height)}
+            if (int(width) > 100):
                 img_list.append(pinfo)
 
     new_html = new_html.replace("src=\"//v.qq.com", "src=\"https://v.qq.com")
@@ -389,24 +401,43 @@ def get_href_detail(html):
     nh = h1 + h3
     '''
 
-    #print new_html
-    #print nh
+    # print new_html
+    # print nh
     return new_html, img_list, author_name, video_list, title
+
 
 def save_h5(html, article_id):
     filename = config.html_path + str(article_id) + '.html'
-    #filename = "./_" + str(article_id) + '.html'
+    # filename = "./_" + str(article_id) + '.html'
     with open(filename, 'w') as f:
         f.write(html)
     return "https://www.qicycling.cn/html/_" + str(article_id) + '.html'
 
+
 def post_to_search(article_id, title, description, h5, author):
     url = "http://47.97.124.47:8085/qicycling_root/api/solr/saveResource"
     headers = {'Content-type': 'application/json'}
-    dict = {"resourceId":str(article_id),"title":title,"description":description,"h5url":h5,"publisher":author,"type":"0"}
+    dict = {"resourceId": str(article_id), "title": title, "description": description, "h5url": h5, "publisher": author,
+            "type": "0"}
     r = requests.post(url, data=json.dumps(dict), headers=headers)
     print r.text
     return r
+
+
+def is_vip_category(cursor, article_id):
+    sql = "select ter.name, r.`object_id`, tax.taxonomy  from wp_term_taxonomy tax inner join wp_terms ter on ter.`term_id` = tax.term_id" \
+          " inner join `wp_term_relationships` r on r.`term_taxonomy_id` = tax.`term_taxonomy_id`" \
+          " where taxonomy = 'category' and r.`object_id` = %d" % article_id
+    cursor.execute(sql)
+    result = cursor_wp.fetchall()
+    list = []
+    for row in result:
+        list.append(row[0])
+    nsync_module_name = u"预览"
+    if nsync_module_name in list:
+        return True
+    return False
+
 
 def recordArticle(db_app, articles):
     cursor_app = db_app.get_cursor()
@@ -418,9 +449,14 @@ def recordArticle(db_app, articles):
             title = article['alt']
             description = article['text']
             href = article['href']
-            print("--------- todo article:%d"%article_id)
+            print("--------- todo article:%d" % article_id)
 
-            cursor_app.execute("SELECT status FROM resource where id=%d"%article_id)
+            # check vip module
+            if is_vip_category(cursor_wp, article_id):
+                print("------- article:{} is vip module, continue".format(article_id))
+                continue
+
+            cursor_app.execute("SELECT status FROM resource where id=%d" % article_id)
             result = cursor_app.fetchone()
             if result is None:
                 html, img_list, author, v_list, t_ = get_href(href)
@@ -439,27 +475,29 @@ def recordArticle(db_app, articles):
                 h5 = save_h5(html, article_id)
 
                 sql = "INSERT INTO resource (id, title, description, status, h5url, is_vip, publisher, type,TIMESTAMP)" \
-                        " VALUES (%d, '%s', '%s', 1, '%s', 0, '%s', 0, %d)" % (article_id, title, description, h5, author, ts*1000)
+                      " VALUES (%d, '%s', '%s', 1, '%s', 0, '%s', 0, %d)" % (
+                          article_id, title, description, h5, author, ts * 1000)
                 cursor_app.execute(sql)
 
-                if(len(v_list)>0):
+                if (len(v_list) > 0):
                     v_url = quote(v_list[0])
-                    #v_url = quote(v_list[0])
-                    sql = "UPDATE resource set video_url='%s' where id=%d"%(v_url,article_id)
-                    #print sql
+                    # v_url = quote(v_list[0])
+                    sql = "UPDATE resource set video_url='%s' where id=%d" % (v_url, article_id)
+                    # print sql
                     cursor_app.execute(sql)
 
                 for i in xrange(len(img_list)):
                     img = img_list[i]
-                    #surl = quote(img['url'])
+                    # surl = quote(img['url'])
                     surl = img['url']
-                    sql = "INSERT INTO resource_image (resource_id, height, `length`, url) values (%d,%d,%d,'%s')"%(article_id,int(img['width']),int(img['height']),surl)
+                    sql = "INSERT INTO resource_image (resource_id, height, `length`, url) values (%d,%d,%d,'%s')" % (
+                        article_id, int(img['width']), int(img['height']), surl)
                     cursor_app.execute(sql)
 
                 # post to
                 post_to_search(article_id, title, description, h5, author)
             else:
-                #print("article_id:%d already exists."%article_id)
+                # print("article_id:%d already exists."%article_id)
                 continue
 
             # 修改赛事类型
@@ -510,6 +548,7 @@ def redo_html(articles):
         html, img_list, author, v_list, t_ = get_href(href)
         h5 = save_h5(html, article_id)
 
+
 # url = "http://www.qicycling.cn"
 # getBanner(url)
 #
@@ -524,26 +563,26 @@ def redo_html(articles):
 
 if __name__ == '__main__':
     # banner
-    #post_to_search(3448, '环阿尔卑斯S5：帕登为巴林美利达车队带来大丰收', '环阿尔卑斯S5：帕登为巴林美利达车队带来大丰收', 'http://www.qicycling.cn/html/_3448.html', 'guyyu')
-    #'''
+    # post_to_search(3448, '环阿尔卑斯S5：帕登为巴林美利达车队带来大丰收', '环阿尔卑斯S5：帕登为巴林美利达车队带来大丰收', 'http://www.qicycling.cn/html/_3448.html', 'guyyu')
+    # '''
     page = 0
-    while(True):
+    while (True):
         page += 1
         articles = getArticle(page)
         if len(articles) <= 0:
             break
         recordArticle(db_app, articles)
-        #redo_html(articles)
-        print("---------------- getArticle page:%d Done."%page)
+        # redo_html(articles)
+        print("---------------- getArticle page:%d Done." % page)
 
     banners = getBanner(config.web_url)
     print banners
     recordBanner(db_app, banners)
-    #'''
-    #print  getArticle(2)
+
+    # '''
+    # print  getArticle(2)
     # 3696 text
     # 3366 video
-    #html, img_list, author, video_list = get_href('http://www.qicycling.cn/3696.html')
-    #print html
+    # html, img_list, author, video_list = get_href('http://www.qicycling.cn/3696.html')
+    # print html
     # print author
-
